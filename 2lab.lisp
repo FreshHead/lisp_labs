b7796b77c10b5b75069d03c6060a71be9309d7c3
Alexander Kostarev <UniverAlex@ya.ru>
2018-01-11 23:33:56 +0300

    Add doc for funtion 'it's second argument must be integer'

--- 2lab.lisp
+++ 2lab.lisp
@@ -1,4 +1,5 @@
 (defun composition(a b)
+	"b must be integer"
 	(cond ((= b 1) a)		
 		(t (+ a (composition a (- b 1) ))) ;(print (+ a (composition a (- b 1) ))))
 	)
@@ -6,11 +7,11 @@
 
 ;(trace composition)
 
-(setq a 83.2)
+(setq a 83.4)
 (setq b 72)
 (setq result (composition a b))
 
-(format t "~%Result of composition ~F and ~F is ~F " a b result)
+(format t "~%Result of composition ~F and ~D is ~F " a b result)
 
 (with-open-file (stream "outdata.txt" :direction :output)
 	(format stream "~F" result)
